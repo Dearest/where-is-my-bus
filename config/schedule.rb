850ee1f66349ebf4ea2f264f6632c903948c0ab7
dearest <iotu.hanchen@gmail.com>
2018-05-11 10:22:46 +0800

    修改定时

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every :weekday, at: ['8:45 am', '8:47 am', '8:49 am', '8:51 am', '8:52 am', '8:53 am', '8:54 am', '8:55 am'] do
-  command "/usr/bin/ruby ~/projects/where-is-my-bus/main.rb company"
+  command "cd ~/projects/where-is-my-bus/ && /usr/bin/ruby main.rb company"
 end
 every :weekday, at: ['6:30 pm', '6:35 pm', '6:40 pm', '6:45 pm', '6:50 pm'] do
   command "/usr/bin/ruby ~/projects/where-is-my-bus/main.rb home"
